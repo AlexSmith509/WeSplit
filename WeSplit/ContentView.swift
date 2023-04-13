734d48356abe0b7f1a81f8f2b6a2ab4b6de2b014
Alex Smith <alexwsmith@me.com>
2023-04-13 15:45:47 -0700

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,77 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Alex Smith on 4/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State  private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocued: Bool
+    let tipPercentages = [10,15,20,25, 0]
+    
+    var totalPerPerson: Double {
+        let PeopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = tipValue + checkAmount
+        let amountPerPerson = grandTotal / PeopleCount
+        
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocued)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                            
+                            
+                                .toolbar {
+                                    ToolbarItem(placement: .keyboard) {
+                                        Button("Done") {
+                                            amountIsFocued = false
+                                        }
+                                    }
+                                }
+                        }
+                    }
+                }
+                Section {
+                    
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Tip")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("We Split")
+        }
+    }
+}
+
+struct ContentViewPreview: PreviewProvider {
+    static var previews: some View {
+        return ContentView()
+    }
+}
+
